9a36d4dc41091ce417aa65c02e67d5d765a739ce
Radu Iscu <linkerro@gmail.com>
2014-07-07 02:06:41 +0300

    Added the parser for binding info in the views

--- test/EnkiSpecs.js
+++ test/EnkiSpecs.js
@@ -1,6 +1,6 @@
-describe('enki binder', function () {
+describe('enki object initializer', function () {
     var testObject = {normalProperty: 1,
-        objectProperty:{nestedProperty:1},
+        objectProperty: {nestedProperty: 1},
         arrayProperty: [1, 2, 3]
     };
     enki.init(testObject);
@@ -23,17 +23,39 @@ describe('enki binder', function () {
         expect(updateResult).toBe('test');
     });
 
-    it('should work for properties that are arrays',function(){
-        var updateResult=0;
-        enki.addListener(testObject, 'normalProperty.nestedProperty', function (value) {
+    it('should work for properties that are arrays', function () {
+        var updateResult = 0;
+        enki.addListener(testObject, 'arrayProperty', function (value) {
             updateResult = value;
         });
-        testObject.arrayProperty=[1,3,5];
-        expect(updateResult).toEqual([1,3,5]);
+        testObject.arrayProperty = [1, 3, 5];
+        expect(updateResult).toEqual([1, 3, 5]);
+        testObject.arrayProperty.push(7);
+        expect(updateResult).toEqual([1, 3, 5, 7]);
     });
 
     it('shouldn\'t affect property behavior', function () {
         testObject.normalProperty = 2;
         expect(testObject.normalProperty).toBe(2);
+    });
+});
+
+describe('enki document binder', function () {
+
+    it('should fail on bad databinding information', function () {
+        setFixtures(sandbox('<div data-bind="click: ,lakjsdflkjf"></div>'));
+        expect(enki.bindDocument).toThrow();
+    });
+
+    it('should parse data-bound items', function () {
+        setFixtures(sandbox('<div data-bind="click: testFunction"></div>'));
+        enki.bindDocument();
+        expect(enki.bindingInfo).toEqual({click:'testFunction'});
+    });
+
+    it('should parse nested objects', function () {
+        setFixtures(sandbox('<div data-bind="style: {backgroundColor:test,padding:5px}"></div>'));
+        enki.bindDocument();
+        expect(enki.bindingInfo).toEqual({style: {backgroundColor: 'test', padding: '5px'}})
     });
 });
