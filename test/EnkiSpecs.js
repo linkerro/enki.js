566a686e74f6c02bfcca54ecb13dc0ca72c9943a
Radu Iscu <linkerro@gmail.com>
2014-07-09 19:51:32 +0300

    Added support for click, one way and two way binding

--- test/EnkiSpecs.js
+++ test/EnkiSpecs.js
@@ -1,61 +1,103 @@
 describe('enki object initializer', function () {
-    var testObject = {normalProperty: 1,
+    var viewModel1 = {normalProperty: 1,
         objectProperty: {nestedProperty: 1},
-        arrayProperty: [1, 2, 3]
+        arrayProperty: [1, 2, 3],
+        testFunction: function () {
+            return true;
+        }
     };
-    enki.init(testObject);
+    enki.watch(viewModel1);
 
     it('should attach functioning notifiers', function () {
         var updateResult = 0;
-        enki.addListener(testObject, 'normalProperty', function (value) {
+        enki.addListener(viewModel1, 'normalProperty', function (value) {
             updateResult = value;
         });
-        testObject.normalProperty = 'test';
+        viewModel1.normalProperty = 'test';
         expect(updateResult).toBe('test');
     });
 
     it('should work for nested properties', function () {
         var updateResult = 0;
-        enki.addListener(testObject, 'objectProperty.nestedProperty', function (value) {
+        enki.addListener(viewModel1, 'objectProperty.nestedProperty', function (value) {
             updateResult = value;
         });
-        testObject.objectProperty.nestedProperty = 'test';
+        viewModel1.objectProperty.nestedProperty = 'test';
         expect(updateResult).toBe('test');
     });
 
     it('should work for properties that are arrays', function () {
         var updateResult = 0;
-        enki.addListener(testObject, 'arrayProperty', function (value) {
+        enki.addListener(viewModel1, 'arrayProperty', function (value) {
             updateResult = value;
         });
-        testObject.arrayProperty = [1, 3, 5];
+        viewModel1.arrayProperty = [1, 3, 5];
         expect(updateResult).toEqual([1, 3, 5]);
-        testObject.arrayProperty.push(7);
+        viewModel1.arrayProperty.push(7);
         expect(updateResult).toEqual([1, 3, 5, 7]);
     });
 
     it('shouldn\'t affect property behavior', function () {
-        testObject.normalProperty = 2;
-        expect(testObject.normalProperty).toBe(2);
+        viewModel1.normalProperty = 2;
+        expect(viewModel1.normalProperty).toBe(2);
     });
 });
 
 describe('enki document binder', function () {
-
+    var viewModel1 = {normalProperty: 1,
+        objectProperty: {nestedProperty: 1},
+        arrayProperty: [1, 2, 3],
+        testFunction: function () {
+            return true;
+        }
+    };
     it('should fail on bad databinding information', function () {
         setFixtures(sandbox('<div data-bind="click: ,lakjsdflkjf"></div>'));
         expect(enki.bindDocument).toThrow();
     });
 
-    it('should parse data-bound items', function () {
+    xit('should parse data-bound items', function () {
         setFixtures(sandbox('<div data-bind="click: testFunction"></div>'));
         enki.bindDocument();
-        expect(enki.bindingInfo).toEqual({click:'testFunction'});
+        expect(enki.bindingInfo).toEqual({click: 'testFunction'});
     });
 
-    it('should parse nested objects', function () {
+    xit('should parse nested objects', function () {
         setFixtures(sandbox('<div data-bind="style: {backgroundColor:test,padding:5px}"></div>'));
         enki.bindDocument();
         expect(enki.bindingInfo).toEqual({style: {backgroundColor: 'test', padding: '5px'}})
+    });
+});
+
+describe('enki bindings', function () {
+    describe('click binding', function () {
+        it('should trigger the clicked event', function () {
+            setFixtures(sandbox('<div id="clickTest" data-bind="click: testFunction"></div>'));
+            var hasClicked = false;
+            var viewModel = {testFunction: function () {
+                hasClicked = true;
+            }};
+            enki.bindDocument(viewModel);
+            document.getElementById('clickTest').onclick();
+            expect(hasClicked).toBe(true);
+        });
+        it ('should trigger one way binding', function () {
+            var viewModel = {normalProperty: 1};
+            setFixtures(sandbox('<div id="bindingTest" data-bind="text: normalProperty"></div>'));
+            enki.bindDocument(viewModel);
+            viewModel.normalProperty = 'lkajsdflkjsf';
+            var element = document.getElementById('bindingTest');
+            expect(element.innerHTML).toBe(viewModel.normalProperty);
+        });
+        it ('should trigger two way binding', function () {
+            var viewModel = {normalProperty: 1};
+            setFixtures(sandbox('<div id="bindingTest" data-bind="text: normalProperty"></div>'+
+                '<input id="bindingTest2" type="text" data-bind="value: normalProperty" />'));
+            enki.bindDocument(viewModel);
+            var element = document.getElementById('bindingTest');
+            var input = document.getElementById('bindingTest2');
+            input.value='asfasdfasfd';
+            expect(element.innerHTML).toBe(input.value);
+        });
     });
 });
