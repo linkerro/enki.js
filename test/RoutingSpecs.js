2a6f02a25d9c626ad871047319d38b86e8be5f1d
Radu Iscu <linkerro@gmail.com>
2014-11-10 22:30:50 +0200

    Made rudementary routing work

--- test/RoutingSpecs.js
+++ test/RoutingSpecs.js
@@ -1,26 +1,65 @@
 describe('enki routing', function () {
-    it('should add routes', function () {
-        var route={
-            name:'default',
-            url:'{area}/{component}'
+
+    var defaultRoute = {
+        name: 'default',
+        url: '{area}/{component}'
+    };
+
+    var defaultUrlExample = 'products/reviews';
+
+    var complexRoute = {
+        url:'{area}/{component}/{id}/reviews/{reviewId}',
+        name:'complexRoute'
+    };
+
+    var complexRouteBadExample = 'buy/products/1234/reviews/244/fghjk';
+    var complexRouteExample = 'buy/products/1234/reviews/244';
+
+    var area,params;
+
+    var component = function (componentContext) {
+        area = componentContext.area;
+
+        return {
+            viewModel: {},
+            template: 'templateName'
         }
-        enki.routing.registerRoute(route);
+    };
+
+
+    beforeEach(function () {
+        enki.routing.clear();
+        area = undefined;
+        params = undefined;
+    });
+
+    afterEach(function () {
+        enki.routing.goToRoot();
+    });
+
+    xit('should add simple routes', function () {
+        enki.routing.registerRoute(defaultRoute);
+    });
+
+    xit('should add complex routes', function () {
+        enki.routing.registerRoute(complexRoute);
     });
 
     it('should register components', function () {
-        var component = function (componentContext) {
-            return {
-                viewModel: {},
-                template: 'templateName'
-            }
-        };
         enki.routing.registerComponent({
             name: 'default',
             component:component
         });
     });
 
-    xit('should identify route specified components', function () {
-
+    it('should identify route specified components', function () {
+        enki.routing.registerRoute(complexRoute);
+        enki.routing.registerComponent({
+            name: 'products',
+            area: 'buy',
+            component: component
+        });
+        enki.routing.changePage(complexRouteExample);
+        expect(area).toBe('buy');
     });
 });
