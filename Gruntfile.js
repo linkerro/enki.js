2a3092b2042bd31be705d95c6f5885bd43ebd753
Radu Iscu <linkerro@gmail.com>
2015-01-07 01:19:49 +0200

    Switched from running test in grunt+jasmine to karma+jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,20 +19,6 @@ module.exports = function (grunt) {
                 src: ['src/*.js', 'test/*.js']
             }
         },
-        jasmine: {
-            options: {
-                specs: 'test/*.js',
-                vendor: 'test/lib/jasmine-2.0.0/jasmine-dom-fixtures.js',
-                host: 'http://127.0.0.1:8000/',
-                keepRunner: false
-            },
-            target: {
-                src: 'src/*.js'
-            }
-        },
-        connect: {
-            server: {}
-        },
         jsdoc: {
             options: {
                 destination: 'doc'
@@ -46,12 +32,8 @@ module.exports = function (grunt) {
     // Load the plugins
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-eslint');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-jsdoc');
 
     // Set up the tasks.
-    grunt.registerTask('default', ['eslint', 'connect', 'jasmine', 'uglify']);
-    grunt.registerTask('test', ['connect', 'jasmine']);
-    grunt.registerTask('fulltest', ['connect', 'jasmine', 'eslint']);
+    grunt.registerTask('default', ['eslint', 'uglify']);
 };
